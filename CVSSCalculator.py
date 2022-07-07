ea540f421e8202a839d77c0119e0b7fe0aa00091
n0t4u <n0t4u@protonmail.com>
2022-07-07 14:15:25 +0200

    Added CVSS Score formulas

--- CVSSCalculator.py
+++ CVSSCalculator.py
@@ -11,7 +11,47 @@ import matplotlib.pyplot as plt
 import re
 import sys
 
+#Variables
+scores={
+	"AV:N":0.85,
+	"AV:A":0.6,
+	"AV:L":0.55,
+	"AV:P":0.2,
+	"AC:L":0.77,
+	"AC:H":0.44,
+	"PR:N":0.85,
+	"PR:LC":0.68, #low and changed
+	"PR:HC":0.5, #high and changed
+	"PR:LU":0.62,
+	"PR:HU":0.27,
+	"UI:N":0.85,
+	"UI:R":0.62,
+	"S:U":1, #Cambiarlo a 0 según fórmula
+	"S:C":1, #Cambiarlo a 0 según fórmula
+	"C:N":0,
+	"C:L":0.22,
+	"C:H":0.56,
+	"I:N":0,
+	"I:L":0.22,
+	"I:H":0.56,
+	"A:N":0,
+	"A:L":0.22,
+	"A:H":0.56
+}
+cvss=[]
+iss= 1-((1-confidenciality)*(1-integrity)*(1-availability))
+impact_sc=7.52*(iss-0.029)-3.25*pow((iss-0.02),15)
+impact_uc=6.42*iss
+exploitability=8.22*av*ac*pr*ui
+baseScore=math.ceil((impact+exploitability)*10)/10 #Original formula= math.floor(old_value * 10**ndecimals) / 10**ndecimals
+
 #Definitions
+def getCVSS():
+	return
+
+def calculateValues():
+	return
+
 def createGraph():
 	labels = ['']
 	impact = [4.2]
@@ -42,9 +82,11 @@ def createGraph():
 	plt.savefig('cvss.png',transparent=True,bbox_inches='tight')
 	plt.show()
 
-#Main
+#Argparse
 
+#Main
 if __name__ == '__main__':
 
 	getCVSS()
+	calculateValues()
 	createGraph()
